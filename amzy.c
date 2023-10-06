6cf45ffebd7be915303570861c59e046ccde38f6
AMZED HOSSAIN <hsamzed786@gmail.com>
2023-10-06 22:51:20 +0530

    factorial

--- amzy.c
+++ amzy.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a,b=1;
+    int a, b = 1;
     printf("enter a\n");
-    scanf("%d",&a);
-    while (a>0){
-        b=b*a;
+    scanf("%d", &a);
+    while (a > 0) {
+        b = b * a;
         a--;
     }
-    printf("the factorial of the number is:%d",b);
+    printf("the factorial of the number is:%d", b);
 }
