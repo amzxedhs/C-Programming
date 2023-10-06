d1789894f9fc6bf3d04b390681f727303293ccf3
AMZED HOSSAIN <hsamzed786@gmail.com>
2023-10-06 11:03:17 +0530

    i do the change coz why not

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
-
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int n=5;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=i;j<=n;j++) {
+            printf("  ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        for(int j=1;j<i;j++ )
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++) {
+            printf("  ");
+        }
+        for(int j=i;j<n;j++)
+        {
+            printf("* ");
+        }
+        for(int j=i;j<=n;j++ )
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
 }
