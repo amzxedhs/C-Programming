90ea9edd0135bc3cc1c82c5761469df6b98728e2
AMZED HOSSAIN <hsamzed786@gmail.com>
2023-10-08 12:42:55 +0530

    degree to radian

---
+++ amzy15.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("enter any angle in degrees");
+    scanf("%f",&a);
+    b=a/180;
+    printf("the value of angle in radian is:%fpi",b);
+}
