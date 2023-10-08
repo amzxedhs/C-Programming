9e2103d7c44a1cb5fdadddcce4194618f95069e9
AMZED HOSSAIN <hsamzed786@gmail.com>
2023-10-08 12:33:19 +0530

    multiplication of number

--- amzy14.c
+++ amzy14.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
+int main(){
+    int a,b=1,c;
+    printf("enter a value");
+    scanf("%d",&a);
 
+    while (a>0,b<=10){
+
+        c=a*b;
+        printf("%d x %d = %d\n",a,b,c);
+        b++;
+    }
+
+
+}
